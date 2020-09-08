7a2548451be00352cf90769a58dd26a35d97c5ab
nelson-singleton <nsingleton@ymail.com>
2020-09-07 19:00:00 -0500

    Built input and event handler.

--- todo/src/App.js
+++ todo/src/App.js
@@ -4,13 +4,33 @@ import './App.css';
 
 
 const Todo = () => {
-  
+
 } 
 
 function App() {
+  const [todo, SetTodo] = useState("")
+  const [state, dispatch] = useReducer(reducer, initialState)
+  console.log(state, todo)
+
+  const handleChanges = e => {
+    SetTodo(e.target.value)
+
+  }
+
   return (
     <div className="App">
-      {initialState.item}
+      <h1>Add to do:</h1>
+      <input
+      type = "text"
+      name = "todo"
+      value = {todo} 
+      onChange = {handleChanges}
+      />
+      <button>Add</button>
+      <button>Clear</button>
+      
+      <h2>To do list</h2>
+      {state.todos.item}
     </div>
   );
 }
